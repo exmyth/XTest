066b2d792a7e10887bf531e8987f399117d76563
exmyth <iexmyth@gmail.com>
2016-01-18 22:27:32 +0800

    customize MyTextLineFactory

--- src/com/exmyth/test/mina/server/MinaServer.java
+++ src/com/exmyth/test/mina/server/MinaServer.java
@@ -3,9 +3,7 @@ package com.exmyth.test.mina.server;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
-import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 import com.exmyth.test.mina.handle.MyServerHandler;
@@ -16,8 +14,8 @@ public class MinaServer {
 		try {
 			NioSocketAcceptor acceptor = new NioSocketAcceptor();
 			acceptor.setHandler(new MyServerHandler());
-			acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory()));
-			acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 5);
+			acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new MyTextLineFactory()));
+//			acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 5);
 			acceptor.bind(new InetSocketAddress(9898));
 		} catch (IOException e) {
 			e.printStackTrace();
